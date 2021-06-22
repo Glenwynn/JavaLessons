233304da61542aea87c3c6caaefa6c69c88c6c15
Glenwynn <sniffka@mail.ru>
2021-06-22 17:14:21 +0300

    домашка

--- Lesson2/src/Lesson2.java
+++ Lesson2/src/Lesson2.java
@@ -1,38 +1,75 @@
 public class Lesson2 {
+    static int a, b;
+
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
+        System.out.println("Задача 1");
+        for (int i = 0; i < 3; i++) {
+            calcAB();
+            System.out.println("Value A:" + a + "Value B:" + b);
+            System.out.println(sum(a, b));
         }
+        System.out.println("Задача 2");
+        calcAB();
+        System.out.println(b);
+        checkNumber(b);
 
+        System.out.println("Задача 3");
+        System.out.println(a);
+        System.out.println(isNegativeOrPositive(a));
 
-    public static void printThreeWords() {
+        System.out.println("Задача 4");
+        printTextNTimes("Хорошо Живем", 5);
 
-        System.out.println("Orange");
-        System.out.println("Apple");
-        System.out.println("Banana");
+        System.out.println("Задача 5");
+        System.out.println(isLeapYear(2020));
     }
 
-    public static void checkSumSign() {
-        int a = (int) (Math.random() * (200 + 1)) - 100;
-        int b = (int) (Math.random() * (200 + 1)) - 100;
-        int c = (a + b);
-        if (c >= 0) {
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
-        }
+    static void calcAB() {
+        a = (int) (Math.random() * (200 + 1)) - 100;
+        b = (int) (Math.random() * (200 + 1)) - 100;
     }
 
-    public static void printColor() {
-        int value = (int) (Math.random() * (200 + 1)) - 100;
-        if (value <= 0) {
-            System.out.println("Красный");
-        } else if (0 < value && value <= 100) {
-            System.out.println("Желтый");
-        } else {
-            System.out.println("Зеленый");
+    //1. Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в
+    // пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
+    public static boolean sum(int x, int y) {
+        return x + y >= 10 && x + y <= 20;
+    }
+
+    //2. Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль,
+    //положительное ли число передали или отрицательное. Замечание: ноль считаем положительным числом.
+    public static void checkNumber(int z) {
+        if (z >= 0)
+            System.out.println("Число положительное");
+        else
+            System.out.println("Число отрицательное");
+
+    }
+
+    //3. Написать метод, которому в качестве параметра передается целое число.
+    // Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
+    public static boolean isNegativeOrPositive(int w) {
+        return w < 0;
+    }
+
+    //    4. Написать метод, которому в качестве аргументов передается строка и число,
+//    метод должен отпечатать в консоль указанную строку, указанное количество раз;
+    public static void printTextNTimes(String text, int n) {
+        for (;n > 0; n--) {
+            System.out.println(text);
         }
+    }
+//    5. * Написать метод, который определяет, является ли год високосным, и возвращает boolean
+//    (високосный - true, не високосный - false).
+//    Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+    public static boolean isLeapYear(int y) {
+        return (y %4 == 0 || y%400 == 0) && y%100 == 0;
     }
 
 }
+
+
+
+
+
+
+
